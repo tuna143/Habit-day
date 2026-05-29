c2f2d7ba9f3ae4d7e65d9c2795e807a61a70dbcd
tuna143 <lucete1229@gmail.com>
2026-05-29 19:28:54 +0900

    t

---
+++ sw.js
@@ -0,0 +1,67 @@
+const cacheName = "habit-day-v4";
+const appShell = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appShell)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const request = event.request;
+  const isNavigation = request.mode === "navigate" || request.destination === "document";
+
+  if (isNavigation) {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(cacheName).then((cache) => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) {
+        fetch(request)
+          .then((response) => {
+            if (response.ok) {
+              caches.open(cacheName).then((cache) => cache.put(request, response.clone()));
+            }
+          })
+          .catch(() => {});
+        return cached;
+      }
+
+      return fetch(request).then((response) => {
+        if (response.ok) {
+          caches.open(cacheName).then((cache) => cache.put(request, response.clone()));
+        }
+        return response;
+      });
+    })
+  );
+});
